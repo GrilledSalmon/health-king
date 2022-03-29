2d7fbc6c8f868bded4010d5ff241b9777b4f2d8b
yoonwoo_labtop <mryoonwoo@naver.com>
2022-03-29 22:47:08 +0900

    회원가입 기본 기능 완

--- app/views/signup_view.py
+++ app/views/signup_view.py
@@ -4,26 +4,23 @@ bp = Blueprint('signup_view',__name__)
 
 @bp.route('/user', methods=['GET', 'POST'])
 def user():
+    """회원가입한 유저 데이터 저장 및 회원가입 페이지 보여주기"""
     if request.method == "POST":
-        # 유저 저장
-        # req = request.form
-        # id = req['id']
-        # password = req['password']
-        # name = req['name']
-        print(request.form)
-        print(type(request.form))
-        # user_collection.insert_one(request.form)
-        user_collection.insert_one
+        # 유저 데이터 저장
+        # doc 형태 : {'id' : id, 'password' : password, 'name' : name}
+        user_collection.insert_one(dict(request.form))
         
         return jsonify({'result' : 'success'})
-    else:
-        return render_template('signup.html')
     
-
+    # GET인 경우    
+    return render_template('signup.html')
     
 
 @bp.route('/user/check', methods=['POST'])
-def user_check():
+def user_id_check():
     # 유저 아이디 중복체크
+    new_id = request.form['id']
+    if user_collection.find_one({'id':new_id}):
+        return jsonify({'result' : 'fail'})
 
-    return render_template('index.html')
+    return jsonify({'result' : 'success'})
