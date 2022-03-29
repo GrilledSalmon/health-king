1a7eafea93f2df804a6625de259b402a2bc10715
gon2gon2 <ark5354@gmail.com>
2022-03-29 16:34:31 +0900

    몽고 db연결

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 from .views import login_view, signup_view, recruit_card_view
+from .secrets import HOST, PORT, USERNAME, PASSWORD
 
+client = MongoClient(
+    HOST,
+    PORT,
+    username=USERNAME,
+    password=PASSWORD
+)
+db = client['health']
+
+user_collection = db.user
+card_collection = db.card
 
 
 app = Flask(__name__)
